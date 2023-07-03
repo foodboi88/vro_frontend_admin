81ba792996d7b9b2e3558553a717c39c19319be2
Trung Hieu Do <92080459+foodboi88@users.noreply.github.com>
2023-07-03 21:32:51 +0700

    set up private page routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import PrivateRoutes from './utils/PrivateRoutes'
 import OutLoginLayout from './layouts/out-login/out-login';
 import InLoginLayout from './layouts/in-login/in-login';
+import General from './pages/general/general';
+import Sketch from './pages/sketch/sketch';
+import User from './pages/user/user';
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route element={<PrivateRoutes />}>
-            <Route element={<InLoginLayout />} path="/management" />
+            <Route element={<InLoginLayout />} path="/management" >
+              <Route element={<General />} path="/management" />
+              <Route element={<Sketch />} path="/management/sketch" />
+              <Route element={<User />} path="/management/user" />
+            </Route>
           </Route>
           <Route element={<OutLoginLayout />} path="/" />
+
         </Routes>
       </Router>
     </div>
---
+++ src/pages/sketch/sketch.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sketch = () => {
+  return (
+    <div>Sketch</div>
+  )
+}
+
+export default Sketch
